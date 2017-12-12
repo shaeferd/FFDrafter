01f815864f04daee6b17f4aca3ac921f7e2210ed
Shaefer Drew <shaeferd@umich.edu>
2017-12-11 20:05:42 -0500

    commit

--- scraper.py
+++ scraper.py
@@ -20,6 +20,56 @@ jsonfile = open('cache.json', 'w')
 
 fieldnames = ('Index','Rank','Player','Position','My Rank','Avg Rank')
 reader = csv.DictReader(csvfile, fieldnames)
+CACHE_DICTION = {}
 CACHE_DICTION['final_rankings'] = [row for row in reader]
-out = json.dumps( [ row for row in reader ], indent = 4)
+csvfile.close()
+
+csvfile = open('avg_draft.csv', 'r')
+
+fieldnames = ('Rank','Player')
+reader = csv.DictReader(csvfile, fieldnames)
+CACHE_DICTION['avg_rankings'] = [row for row in reader]
+csvfile.close()
+
+csvfile = open('d.csv', 'r')
+
+fieldnames = ('Player','TT','SCK','FF','FR','INT','ITD','FTD','PTS')
+reader = csv.DictReader(csvfile, fieldnames)
+CACHE_DICTION['d_rankings'] = [row for row in reader]
+csvfile.close()
+
+csvfile = open('k.csv', 'r')
+
+fieldnames = ('Player','R1','R2','R3','TOT','XP','PTS')
+reader = csv.DictReader(csvfile, fieldnames)
+CACHE_DICTION['k_rankings'] = [row for row in reader]
+csvfile.close()
+
+csvfile = open('qb.csv', 'r')
+fieldnames = ('Rank','Player','Position','CA','Pass_YDS','Pass_TD','INT','RUSH','Rush_YDS','Pass_TD','REC','Rec_YDS','Rec_TD','PTS')
+reader = csv.DictReader(csvfile, fieldnames)
+CACHE_DICTION['qb_rankings'] = [row for row in reader]
+csvfile.close()
+
+csvfile = open('rb.csv', 'r')
+fieldnames = ('Player','CA','Pass_YDS','Pass_TD','INT','RUSH','Rush_YDS','Pass_TD','REC','Rec_YDS','Rec_TD','PTS')
+reader = csv.DictReader(csvfile, fieldnames)
+CACHE_DICTION['rb_rankings'] = [row for row in reader]
+csvfile.close()
+
+csvfile = open('te.csv', 'r')
+fieldnames = ('Player','CA','Pass_YDS','Pass_TD','INT','RUSH','Rush_YDS','Pass_TD','REC','Rec_YDS','Rec_TD','PTS')
+reader = csv.DictReader(csvfile, fieldnames)
+CACHE_DICTION['te_rankings'] = [row for row in reader]
+csvfile.close()
+
+csvfile = open('wr.csv', 'r')
+fieldnames = ('Player','CA','Pass_YDS','Pass_TD','INT','RUSH','Rush_YDS','Pass_TD','REC','Rec_YDS','Rec_TD','PTS')
+reader = csv.DictReader(csvfile, fieldnames)
+CACHE_DICTION['wr_rankings'] = [row for row in reader]
+csvfile.close()
+
+out = json.dumps(CACHE_DICTION, indent = 4)
 jsonfile.write(out)
+
+
